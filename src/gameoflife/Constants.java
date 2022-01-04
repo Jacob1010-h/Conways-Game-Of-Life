70f328a052d1a823fee730e4b48e4b5a8dca5c29
Jacob1010-h <lordgaming1010@gmail.com>
2022-01-04 22:01:15 +0000

    message: changes move to block/position

--- src/gameoflife/Constants.java
+++ src/gameoflife/Constants.java
@@ -29,10 +29,10 @@ public class Constants
     public static final String WELCOME_TITLE = "Welcome to The Game of Life!";
     public static final String TITLE = "Thanks for playing the Game Of Life!";
     public static final String INVALID_ROW_OR_COLUMN = "Your row and column must be 1, 2, or 3";
-    public static final String GET_ROW_MOVE = "Player %s: Enter the row for your next move";
-    public static final String GET_COL_MOVE = "Player %s: Enter the column for your next move";
-    public static final String INVALID_MOVE_ERROR = "Row %d column %d is not a valid move. Please try again";
+    public static final String GET_ROW_MOVE = "Player %s: Enter the row for your next block";
+    public static final String GET_COL_MOVE = "Player %s: Enter the column for your next block";
+    public static final String INVALID_MOVE_ERROR = "Row %d column %d is not a valid position. Please try again";
     public static final String CHECK_STOP_STR = "Do you want to run the program? y/n";
-    public static final String PRINT_MOVE = "Player %s move to %d %d";    
-    public static final String START_NEW_GAME = "Start a new game (Y or N)? -->";
+    public static final String PRINT_MOVE = "Player %s block to %d %d";    
+    public static final String START_NEW_GAME = "Start a new simulation (Y or N)? -->";
 }
